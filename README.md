deb707653d7e1568dd730a4cd885aa82b8023210
Andrey Maksimov <aamaksimov@ctcmedia.ru>
2022-05-24 15:54:23 +0300

    Add readme

---
+++ README.md
@@ -0,0 +1,19 @@
+# Openapi2conv
+
+Конвертер для преобразования OpenAPI версии 2 в OpenAPI версии 3.
+
+## Использование
+
+```bash
+$ openapi2conv input_json_file output_json_file
+```
+
+## Разработка
+
+Вам понадобится установленный [Golang](https://go.dev).
+
+```bash
+$ git clone gitlab.ctcmedia.ru/MoreTV/go-openapi-converter
+$ cd go-openapi-converter
+$ go build openapi2conv
+```
---
+++ openapi2conv.go
@@ -0,0 +1,76 @@
+package openapi2conv
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"reflect"
+	"strings"
+
+	"github.com/getkin/kin-openapi/openapi2"
+	"github.com/ghodss/yaml"
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	var rootCmd = &cobra.Command{
+		Use:   "openapi2conv <input.json> <output.yaml>",
+		Short: "Gopher CLI in Go",
+		Long:  `Gopher CLI application written in Go.`,
+		Args:  cobra.MinimumNArgs(2),
+		// Uncomment the following line if your bare application
+		// has an action associated with it:
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Print: " + strings.Join(args, " "))
+			convert(args[0], args[1])
+		},
+	}
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func convert(input_json, output_yaml string) {
+	input, err := os.ReadFile(input_json)
+	if err != nil {
+		panic(err)
+	}
+
+	var doc openapi2.T
+	if err = json.Unmarshal(input, &doc); err != nil {
+		panic(err)
+	}
+
+	outputJSON, err := json.Marshal(doc)
+	if err != nil {
+		panic(err)
+	}
+	var docAgainFromJSON openapi2.T
+	if err = json.Unmarshal(outputJSON, &docAgainFromJSON); err != nil {
+		panic(err)
+	}
+	if !reflect.DeepEqual(doc, docAgainFromJSON) {
+		fmt.Println("objects doc & docAgainFromJSON should be the same")
+	}
+
+	outputYAML, err := yaml.Marshal(doc)
+	if err != nil {
+		panic(err)
+	}
+	var docAgainFromYAML openapi2.T
+	if err = yaml.Unmarshal(outputYAML, &docAgainFromYAML); err != nil {
+		panic(err)
+	}
+	if !reflect.DeepEqual(doc, docAgainFromYAML) {
+		fmt.Println("objects doc & docAgainFromYAML should be the same")
+	}
+
+	// Write outputYAML to file
+	if err := os.WriteFile(output_yaml, outputYAML, 0644); err != nil {
+		panic(err)
+	}
+
+	fmt.Print("Successfully converted " + input_json + " to " + output_yaml)
+}
